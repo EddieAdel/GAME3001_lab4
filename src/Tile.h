2faf6325687ebbdbd5e6928d6e0713d8c2c0057c
Eddie <aeddie2001@gmail.com>
2021-02-11 16:50:24 -0500

    GAME3001
    lab 4

---
+++ src/Tile.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef __TILE__
+#define __TILE__
+#include "DisplayObject.h"
+#include "Label.h"
+#include "NeighbourTile.h"
+
+class Tile : public DisplayObject
+{
+public:
+	// Constructor
+	Tile();
+	
+	// Destructor
+	~Tile();
+	
+	// Life-Cycle Functions
+	void draw() override;
+	void update() override;
+	void clean() override;
+
+	Tile* getNeighbourTile(NeighbourTile position);
+	void setNeighbourTile(NeighbourTile position, Tile* tile);
+
+	float getTileCost() const;
+	void setTileCost(float cost);
+
+	void addLabels();
+	void setLabelsEnabled(bool state);
+
+	glm::vec2 getGridPosition() const;
+	void setGridPosition(const float col, const float row);
+
+private:
+
+	Label* m_costLabel;
+	Label* m_statusLabel;
+	float m_cost;
+	Tile* m_neighbours[NUM_OF_NEIGHBOUR_TILES];
+	glm::vec2 m_gridPosition;
+};
+
+#endif /* defined (__TILE__) */
